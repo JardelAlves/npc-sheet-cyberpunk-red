0a869e945d6fcc6a197709d6d06d29c5901ba88d
JardelAlves <jardelfilho2001@gmail.com>
2023-10-08 16:01:19 -0300

    Deploy config

--- Cyberpunk Red NPC Sheet/vite.config.ts
+++ Cyberpunk Red NPC Sheet/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/npc-sheet-cyberpunk-red',
   plugins: [vue()],
   resolve: {
     alias: {
