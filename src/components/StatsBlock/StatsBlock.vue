e1c188eb28427bdad45d563e1827bb6126a7d1ab
jardelalves <jardelfilho2001@gmail.com>
2024-06-13 13:45:42 -0300

    remaking project with different framework

---
+++ src/components/StatsBlock/StatsBlock.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <q-input
+      v-model="text"
+      label="INT"
+      maxlength="2"
+      outlined
+      counter
+      class="clear-arrows"
+      hide-bottom-space
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const text = ref('0')
+console.log(text)
+</script>
+
+<style scoped>
+</style>
